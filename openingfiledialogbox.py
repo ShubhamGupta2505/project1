b00cf3b219467b9870788d77f4e5e14095d364f4
Shubham <shubham3nemo@gmail.com>
2021-06-03 12:39:20 +0530

    changing title of filedialogbox

--- openingfiledialogbox.py
+++ openingfiledialogbox.py
@@ -3,7 +3,7 @@ from PIL import ImageTk,Image
 from tkinter import filedialog
 
 root=Tk()
-root.title('hiiiiiii')
+root.title('hiii')
 root.iconbitmap('c:/Users/gf/Downloads/abc.ico')
 
 root.filename = filedialog.askopenfilename(initialdir="c:/Users/gf/Downloads",title="SElect a File  :",filetypes=(("png file ","*.png"),("jpg files","*.jpg")))
